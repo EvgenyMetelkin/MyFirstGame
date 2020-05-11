8cce830324b55f4eb6d7386b2c567d31e88f63b3
Evgeny <metelkin.evgen@yandex.ru>
2020-05-11 22:11:24 +0300

    First version game

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform playerPosition;
+    Vector3 position;
+    float previosY;
+    float speedCamer = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        position.x = 0f;
+        position.y = 0f;
+        position.z = -10f;
+        transform.position = position;
+
+        previosY = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = playerPosition.position;
+        position.x = 0f;
+        position.z = -10f;
+        if(position.y < 0) 
+            position.y = 0f;
+        if (position.y < previosY)
+            position.y = previosY;
+
+        position.y += 0.004f;
+        previosY = position.y;
+        transform.position = Vector3.Lerp(transform.position, position, speedCamer * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flip(); 
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    /////////////////////////////////////// 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            Jump();
+        }
+    }
+    void Move()
+    {
+        rb.velocity = new UnityEngine.Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = UnityEngine.Quaternion.Euler(0, 0, 0);
+        else if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = UnityEngine.Quaternion.Euler(0, 200, 0);
+    }
+     
+    void Jump()
+    {
+        rb.velocity = UnityEngine.Vector2.zero;
+        rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        animator.SetInteger("State", 1);
+    }
+}
