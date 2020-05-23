403aae1305d600c9152c9bdd70fd6d9dce923c6b
Evgeny <metelkin.evgen@yandex.ru>
2020-05-23 12:11:23 +0300

    Add update position wall

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    Camera m_MainCamera;
+    public int magicCameraHeight; 
+    void Start()
+    {
+        m_MainCamera = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+     
+    void Update()
+    {
+        UpdateWallPosition(); 
+    }
+
+    void UpdateWallPosition()
+    {
+        if (m_MainCamera.transform.position.y - transform.position.y > magicCameraHeight)
+        {
+            Instantiate(gameObject, new Vector2(transform.position.x, m_MainCamera.transform.position.y), transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
